34eb9f819795f31a7c59c5c13666a59fa7776955
Mike Lee <git@iskitz.com>
2016-11-13 16:41:35 -0800

    +/namae: code simplification + "get" experimentation/

--- namae/namae.js
+++ namae/namae.js
@@ -5,10 +5,15 @@
     { id: "namae@ionify"
     , is: "A Japanese name creator"
     , by: "mike.lee@ionify"
-    , at: "2016.11.06-07"
+    , at: "2016.11.13-08...06"
     , in: "san-jose.ca.usa.earth"
     },
- 
+/*
+  do:
+    [ {get:"view"}
+    , {get:"...", in:"kana", for:"namae", then:"makeNames"}
+    ],
+*/
   get:
     ["kana.js", "view.js"],
 
@@ -16,35 +21,30 @@
     ["kana"],
  
   kana:
-    function kana(ion)
-      { var names = []
-          , hira  = ion.kana.hiragana
-          , kata  = ion.kana.katakana
-          , roma  = ion.kana.romaji
-          , namae = kana.namae
-          ;
-         namae.hira = hira;
-         namae.kata = kata;
-         namae.roma = roma;
-         namae.spot = [0,1];
+    function onKana (ion)
+      { var namae          = onKana.namae
+          , kana           = ion.kana
+          ; namae.hiragana = kana.hiragana
+          ; namae.katakana = kana.katakana
+          ; namae.romaji   = kana.romaji
       },
  
   spot:
-    function onSpot(ion) // +{spot: [0, 1]}
+    function onSpot (ion)            // ion: +{spot: [0, 1]}
       { var spot    = ion.spot
           , alpha   = spot[0]
           , len     = spot[1]
           , namae   = onSpot.namae
-          , which   = alpha % namae.roma.length
-          , roma    = namae.roma[which]
-          , kata    = namae.kata[which]
-          , hira    = namae.hira[which]
-         ;
-        // generate name by alpha-length based on spot
-        
-       
-        
-      }
+          , which   = alpha % namae.romaji.length
+          , roma    = namae.romaji   [which]
+          , kata    = namae.katakana [which]
+          , hira    = namae.hiragana [which]
+          ; namae.spot = spot
+          ;
+
+        // td: generate names by alpha-length based on spot
+      },
+
   do:
     [ {get:"view"}
     , {get:"kana", then:"makeNames"}
@@ -52,12 +52,12 @@
  
   makeNames:
     function makeNames ()
-      {  var my   = makeNames.my
-           , kana = my.kana
-           ;
+      { var my   = makeNames.namae
+          , kana = my.kana
+          ;
 
          //td: create names using 2-3 characters
-      },
+      }
     
 }
 // Approach
